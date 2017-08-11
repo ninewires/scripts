a98f107534645883bea0d0419637bf1bfc89f388
ninewires <ashtonjwa@gmail.com>
2017-08-10 21:23:44 -0400

    Added check for Windows file type & prompt to convert.

--- iplist-gen.sh
+++ iplist-gen.sh
@@ -4,7 +4,7 @@
 # File may contain:
 #	Single IP: 192.168.1.100
 #	Range: 192.168.1.1-192.168.1.15
-#	CIDR: 192.168.1.0/24
+#	CIDR: 192.168.1.0/24			## Broadcast ID & Network ID are removed
 #
 # Usage: <script_name> <ip_file>
 #
@@ -17,6 +17,12 @@ TIME=$(date +"%H%M%S")
 filename="iplist-$TIME.txt"
 inputfile=$1
 
+GRN='\x1B[1;32m'
+WHT='\x1B[1;37m'
+RED='\x1B[1;31m'
+YEL='\x1B[1;33m'
+NC='\x1B[0m'
+
 # Argument Check & Usage ###########################################################
 
 sname=$(basename "$0")
@@ -28,13 +34,27 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-# Input Validation #################################################################
+# File Type Check ##################################################################
 
-GRN='\x1B[1;32m'
-WHT='\x1B[1;37m'
-RED='\x1B[1;31m'
-YEL='\x1B[1;33m'
-NC='\x1B[0m'
+if file $inputfile | grep 'with CRLF line terminators' 1>/dev/null; then
+	echo
+	echo -e "eeeeew ${WHT}$inputfile ${NC}is a windoze format"
+	if ! locate dos2unix 1>/dev/null; then
+		echo
+		echo -e "Looks like you don't have ${WHT}dos2unix ${NC}on your system :-/"
+		echo
+		echo -e "Get ${WHT}$inputfile ${NC}converted and try again."
+		exit 1
+	else
+		echo
+		echo -e "Looks like you've got ${WHT}dos2unix ${NC}installed though \o/"
+		echo
+		echo -e "Get ${WHT}$inputfile ${NC}converted and try again."
+		exit 1
+	fi
+fi
+
+# Input Validation #################################################################
 
 CNT=0
 
@@ -165,8 +185,8 @@ while read input; do
 						oct3low=${addr2[2]}
 						oct3high=${addr2[2]}
 						oct4=$(( (2**$bits - 1) | bc ))
-						oct4low=$(( (${addr2[3]} + 1) ))               #Remove Network ID
-						oct4high=$(( ((${addr2[3]} + $oct4) -1) ))     #Remove Broadcast ID
+						oct4low=$(( (${addr2[3]} + 1) ))               # Remove Network ID
+						oct4high=$(( ((${addr2[3]} + $oct4) -1) ))     # Remove Broadcast ID
 					fi
 				fi
 			fi
@@ -187,11 +207,11 @@ while read input; do
 
 done < $inputfile
 
-cat tmpiplistgen.txt | sort -uV >> $filename
+sort tmpiplistgen.txt -uV >> $filename
 rm tmpiplistgen.txt 2>/dev/null
 
 hostcount=$(cat $filename | wc -l)
 echo
-echo -e "See ${WHT}$filename${NC} in Current Working Dir"
-echo "  $hostcount Total Addresses"
+echo -e "See ${WHT}$filename ${NC}in Current Working Dir"
+echo "  ${WHT}$hostcount ${NC}Total Addresses"
 echo
